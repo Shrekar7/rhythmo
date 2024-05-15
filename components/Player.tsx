0ed8a9d0131f2a605f6ed0724d5f41227f7c5d59
shrekar <shrekarreddy7@gmail.com>
2024-05-15 09:53:52 +0530

    final

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import useGetSongById from "@/hooks/useGetSongById";
-import useLoadSongUrl from "@/hooks/useLoadSongurl";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 import PlayerContent from "./PlayerContent";
 
